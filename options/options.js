6d7e28ce64fcc4f715e0705ce90536af23a81f01
Crowdbotics <team@crowdbotics.com>
2024-02-05 01:10:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-hill-45837",
+  url: "https://proud-hill-45837.botics.co",
+  api: "https://proud-hill-45837.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
